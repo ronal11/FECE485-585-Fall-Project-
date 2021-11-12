0402249e09d1e78c68af44b1aed6b0f3d0c77595
jonathans2017 <stanescu@pdx.edu>
2021-11-11 17:08:43 -0800

    added queue code to main of checkpoint2 file

--- checkpoint_2/checkpoint2.c
+++ checkpoint_2/checkpoint2.c
@@ -16,44 +16,27 @@
 
 // Global variable
 int DEBUG;  //DEBUG program if 1, otherwise set to 0.
+int clk;
 
- void readInputDataFile(FILE *filePointer, unsigned long long inputData[100][3], int *rowsPtr) {
-    //This function reads an input file to extract the data in an array called inputData.
-    // The text file has the format <time><operation><hexadecimal address>.
+//For the queue------------------------------------------------------------------------------------------
+struct node
+{
+    int data;
+//     DATA data;
+    struct node *next;
+};
 
-    char lineValues[30]; //This string will hold a single line at a time in the text file.
-    unsigned long long hexNum; //This variable is 64bits to read the hex value from the text file.
+struct node *front = NULL;
+struct node *rear = NULL;
 
-    //Loop every line in the file to extract data until NULL is returned, indicating end of the file.
-    int i = 0; //This variable holds how many rows (or lines) we go through.
-    while (1) {
-        if (fgets(lineValues, 30, filePointer) != NULL) {
-            char *token = strtok(lineValues, " "); //Create a pointer called token to hold the individual items in the string array
-                                               // from a single line.  This will create 3 separated tokens for time, operation, & hex address.
-            int k = 0;  //This variable indicates the column index (are we reading time, operation, or hex address?).
-            while (token != NULL) {
-                if ( k == 2) { //Special case if we are reading the hex address token.
-                    sscanf(token, "%I64X", &hexNum);
-                    // printf("lineNum:%I64u.\n", hexNum); //Test
-                    inputData[i][k++] = hexNum;
-                }
-                else {
-                    sscanf(token, "%I64u", &inputData[i][k++]);
-                }
-                token = strtok(NULL, " ");
-            }
-            i = i + 1; //Increment the row.
-        }
-        else {
-            *rowsPtr = i;
-            if (DEBUG) {
-                printf("%d lines read in total!\n", *rowsPtr);
-            }
-            break;
-        }
-    }
- }
+void display();
+void enqueue(int);
+void dequeue();
+//------------------------------------------------------------------------------------------
 
+//For the trace file array information
+ void readInputDataFile(FILE *filePointer, unsigned long long inputData[100][3], int *rowsPtr);
+ //------------------------------------------------------------------------------------------
 
 int main()
 {
@@ -100,7 +83,95 @@ int main()
         }
     }
 
+    //Begin clock clock cycles.
+    clk = 0;
+    while(1) {
+
+    }
+
     fclose(filePointer);
 
     return 0;
+}
+
+ void readInputDataFile(FILE *filePointer, unsigned long long inputData[100][3], int *rowsPtr) {
+    //This function reads an input file to extract the data in an array called inputData.
+    // The text file has the format <time><operation><hexadecimal address>.
+
+    char lineValues[30]; //This string will hold a single line at a time in the text file.
+    unsigned long long hexNum; //This variable is 64bits to read the hex value from the text file.
+
+    //Loop every line in the file to extract data until NULL is returned, indicating end of the file.
+    int i = 0; //This variable holds how many rows (or lines) we go through.
+    while (1) {
+        if (fgets(lineValues, 30, filePointer) != NULL) {
+            char *token = strtok(lineValues, " "); //Create a pointer called token to hold the individual items in the string array
+                                               // from a single line.  This will create 3 separated tokens for time, operation, & hex address.
+            int k = 0;  //This variable indicates the column index (are we reading time, operation, or hex address?).
+            while (token != NULL) {
+                if ( k == 2) { //Special case if we are reading the hex address token.
+                    sscanf(token, "%I64X", &hexNum);
+                    // printf("lineNum:%I64u.\n", hexNum); //Test
+                    inputData[i][k++] = hexNum;
+                }
+                else {
+                    sscanf(token, "%I64u", &inputData[i][k++]);
+                }
+                token = strtok(NULL, " ");
+            }
+            i = i + 1; //Increment the row.
+        }
+        else {
+            *rowsPtr = i;
+            if (DEBUG) {
+                printf("%d lines read in total!\n", *rowsPtr);
+            }
+            break;
+        }
+    }
+ }
+
+ void enqueue(int item)
+{
+    struct node *nptr = malloc(sizeof(struct node));
+    nptr->data = item;
+    nptr->next = NULL;
+    if (rear == NULL)
+    {
+        front = nptr;
+        rear = nptr;
+    }
+    else
+    {
+        rear->next = nptr;
+        rear = rear->next;
+    }
+}
+
+void display()
+{
+    struct node *temp;
+    temp = front;
+    printf("\n");
+    while (temp != NULL)
+    {
+        printf("%d\t", temp->data);
+        temp = temp->next;
+    }
+}
+
+void dequeue()
+{
+    if (front == NULL)
+    {
+        printf("\n\nqueue is empty \n");
+    }
+    else
+    {
+        struct node *temp;
+        temp = front;
+        front = front->next;
+        printf("\n\n%d deleted", temp->data);
+        free(temp);
+    }
 }
